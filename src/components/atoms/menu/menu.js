36d70c01ac7a9e8692ba6e17ffcacaffa832dfd8
Gallaann <reindzer26rus@gmail.com>
2023-10-01 23:08:33 +0300

    Feature: add base layout and sidebar

---
+++ src/components/atoms/menu/menu.js
@@ -0,0 +1,58 @@
+'use strict';
+
+import {BaseComponent} from "../../baseComponent.js";
+
+const DEFAULT_LIST = {
+    menuSections: [
+        {
+            menuSectionHeading: "Heading",
+            menuItems: [
+                {
+                    menuItemIcon: "",
+                    menuItemAlt: "",
+                    menuItemText: "Menu button",
+                    menuItemId: "",
+                }
+            ]
+        }
+    ]
+}
+
+/**
+ * Class representing a Menu component.
+ * @extends BaseComponent
+ */
+export class Menu extends BaseComponent {
+
+    /**
+     * Create a Menu component.
+     * @param {Object} [state=DEFAULT_LIST] - The initial state of the Menu component.
+     * @param {HTMLElement} parent - The parent HTML element where the Menu will be rendered.
+     */
+    constructor(state = DEFAULT_LIST, parent) {
+        super(state, parent);
+    }
+
+    /**
+     * Render the Menu component's template to the parent element.
+     */
+    renderTemplateToParent() {
+        const templatesToStateMap = {
+            'menu.hbs': this.getState(),
+        };
+
+        super.renderTemplateToParent(templatesToStateMap);
+    }
+
+    /**
+     * Render the Menu component.
+     * @returns {HTMLElement} The rendered Menu element.
+     */
+    render() {
+        const templatesToStateMap = {
+            'menu.hbs': this.getState(),
+        };
+
+        return super.render(templatesToStateMap);
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,30 +1,6 @@
-import {Card} from "./components/molecules/card/card.js";
-import {List} from "./components/atoms/list/list.js";
+import {Layout} from "./pages/layout/layout.js";
 
-const listItems = {
-    listItems: [
-        {
-            listItemTitle: 'text1',
-            listItemValue: '2',
-            valueUnits: 'Р',
-        },
-        {
-            listItemTitle: 'text2',
-            listItemValue: '2',
-            valueUnits: 'Р',
-        }
-    ]
-};
+const parentElement = document.querySelector('#root');
 
-const list = new List(listItems);
-
-const parentElement = document.getElementById('root');
-const card = new Card({
-    cardSize: 'card-small',
-    cardHeadline: 'Example',
-    cardSubhead: 'Subhead',
-    cardStatus: 'card__status_bad',
-    list: list.render(),
-}, () => {
-}, parentElement);
-card.renderTemplateToParent();
+const layout = new Layout(parentElement,);
+layout.renderTemplateToParent()
