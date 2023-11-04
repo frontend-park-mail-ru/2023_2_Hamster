96fc7381efe3bccf914961e792dbe907c316208d
Gallaann <reindzer26rus@gmail.com>
2023-11-05 02:20:46 +0300

    Add card button component

---
+++ src/components/atoms/button_card/buttonCard.js
@@ -0,0 +1,125 @@
+import { BaseComponent } from '@components/baseComponent.js';
+
+import template from './buttonCard.hbs';
+import { SVG_ICONS } from '@icons/icons';
+
+const DEFAULT_CARD = {
+    cardColor: 'button-card_accent-color',
+    description: 'Ваш план',
+    text: 'Базовый',
+    path: SVG_ICONS.box.path,
+};
+
+/**
+ * Represents a small Card component.
+ *
+ * @class
+ */
+export class ButtonCard extends BaseComponent {
+    /**
+     * The function that will handle the click event of the small Card element.
+     * @type {Function}
+     */
+    #clickHandler = () => {
+    };
+
+    /**
+     * Creates an instance of small card.
+     *
+     * @constructor
+     * @param {HTMLElement} parent - The parent element where the card will be rendered.
+     * @param {Object} state - The initial state of the card component. (optional)
+     * @param {Function} [clickHandler = this.#clickHandler] - The function that will handle the click event of the card element. (optional)
+     */
+    constructor(parent, state = DEFAULT_CARD, clickHandler) {
+        state = { ...DEFAULT_CARD, ...state };
+        super(state, null, parent);
+
+        if (typeof clickHandler === 'function') {
+            this.#clickHandler = clickHandler;
+        }
+    }
+
+    /**
+     * Renders the card component and inserts into parent.
+     *
+     * @returns {string} - The rendered HTML template of the card.
+     * @function
+     */
+    async renderTemplateToParent() {
+        return await super.renderTemplateToParent([template(this.getState())]);
+    }
+
+    /**
+     * Renders the card component.
+     *
+     * @returns {string} - The rendered HTML template of the card.
+     * @function
+     */
+    render() {
+        return super.render([template(this.getState())]);
+    }
+
+    /**
+     * Remove listeners and then set them up.
+     *
+     * @function
+     */
+    cleanUp() {
+        const avatarElement = document.querySelector(this.getState().id);
+        if (avatarElement) {
+            avatarElement.removeEventListener('click', this.getHandler());
+        }
+
+        this.setHandlers();
+    }
+
+    /**
+     * Setup handlers after rendering template.
+     *
+     * @function
+     */
+    setHandlers() {
+        if (!this.parent) {
+            console.error(`can't set handlers for ${this} because no parent`);
+            return;
+        }
+
+        const avatarElement = this.parent.querySelector(this.getState().id);
+        if (avatarElement) {
+            avatarElement.addEventListener('click', this.handleClick.bind(this));
+        }
+    }
+
+    /**
+     * Handles the click event of the image element.
+     *
+     * @function
+     * @param {Event} event - The click event object.
+     */
+    handleClick(event) {
+        if (typeof this.#clickHandler === 'function') {
+            this.#clickHandler();
+        }
+    }
+
+    /**
+     * Sets the click event handler for the image.
+     *
+     * @function
+     * @param {Function} clickHandler - The function to handle the click event.
+     */
+    setHandler(clickHandler) {
+        this.#clickHandler = clickHandler;
+    }
+
+    /**
+     * Gets the current click event handler for the image.
+     *
+     * @function
+     * @returns {Function} - The current click event handler.
+     */
+    getHandler() {
+        return this.#clickHandler;
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,28 +1,32 @@
 import './index.scss';
+import { ButtonCard } from '@atoms';
 
-import { LoginSignupView, DashboardView } from '@views';
-import { router } from '@router';
-import { ROUTE_CONSTANTS } from '@constants/constants';
-import { Layout } from '@organisms/layout/layout';
-
+// import { LoginSignupView, DashboardView } from '@views';
+// import { router } from '@router';
+// import { ROUTE_CONSTANTS } from '@constants/constants';
+// import { Layout } from '@organisms/layout/layout';
+//
 const root = document.querySelector('#root');
+//
+// // TODO: move all routes to specified file
+// const routes = {
+//     [ROUTE_CONSTANTS.LOGIN_ROUTE]: {
+//         view: new LoginSignupView(root, true),
+//     },
+//     [ROUTE_CONSTANTS.REGISTRATION_ROUTE]: {
+//         view: new LoginSignupView(root, false),
+//     },
+//     [ROUTE_CONSTANTS.HOME_ROUTE]: {
+//         view: new Layout(root, undefined, new DashboardView(null)),
+//     },
+//     [ROUTE_CONSTANTS.DASHBOARD_ROUTE]: {
+//         view: new Layout(root, undefined, new DashboardView(null)),
+//     },
+// };
+//
+// router.addRoutes(routes);
+//
+// router.start();
 
-// TODO: move all routes to specified file
-const routes = {
-    [ROUTE_CONSTANTS.LOGIN_ROUTE]: {
-        view: new LoginSignupView(root, true),
-    },
-    [ROUTE_CONSTANTS.REGISTRATION_ROUTE]: {
-        view: new LoginSignupView(root, false),
-    },
-    [ROUTE_CONSTANTS.HOME_ROUTE]: {
-        view: new Layout(root, undefined, new DashboardView(null)),
-    },
-    [ROUTE_CONSTANTS.DASHBOARD_ROUTE]: {
-        view: new Layout(root, undefined, new DashboardView(null)),
-    },
-};
-
-router.addRoutes(routes);
-
-router.start();
+const card = new ButtonCard(root, undefined, undefined);
+card.renderTemplateToParent();
