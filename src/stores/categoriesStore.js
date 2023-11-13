e3003e4811b006b25affcf5c51bf332ffbd234c9
Gallaann <reindzer26rus@gmail.com>
2023-11-13 04:23:53 +0300

    fix rerender after update

--- src/stores/categoriesStore.js
+++ src/stores/categoriesStore.js
@@ -90,7 +90,7 @@ class CategoriesStore extends BaseStore {
         try {
             const response = await categoryApi.updateTag(this.storage.tags[0].id, data);
 
-            this.storage.states = this.storage.states.map(item => {
+            this.storage.states.map(item => {
                 if (item.raw === response.id) {
                     return {
                         id: 'id' + response.id,
@@ -101,6 +101,7 @@ class CategoriesStore extends BaseStore {
                 }
                 return item;
             });
+            console.log(this.storage.states);
             this.storeChanged = true;
 
             this.emitChange(EVENT_TYPES.RERENDER_CATEGORIES);
