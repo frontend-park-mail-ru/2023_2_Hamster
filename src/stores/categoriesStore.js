5ad09bb17685bfb7d58b10308f20d296e3397851
Gallaann <reindzer26rus@gmail.com>
2023-11-13 03:35:18 +0300

    fix rerender after delete categories

--- src/stores/categoriesStore.js
+++ src/stores/categoriesStore.js
@@ -76,7 +76,7 @@ class CategoriesStore extends BaseStore {
         try {
             await categoryApi.deleteTag(data);
 
-            this.storage.states.filter(item => item.id !== data.id);
+            this.storage.states.filter(item => item.id !== 'id' + data.id);
             this.storeChanged = true;
 
             this.emitChange(EVENT_TYPES.RERENDER_CATEGORIES);
